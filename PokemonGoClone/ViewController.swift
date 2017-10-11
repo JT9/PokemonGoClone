6994695cfaf2cca791e91129ad5ef442621b8162
C T <caleb.tsosie@asu.edu>
2017-10-11 02:58:45 -0700

    Added Func to update user location

--- PokemonGoClone/ViewController.swift
+++ PokemonGoClone/ViewController.swift
@@ -9,17 +9,37 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
+    var updateCount = 0
+    
+    //Responsible for tracking where the user is
+    var manager = CLLocationManager()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        manager.delegate = self
         
-        mapView.showsUserLocation = true
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            print("Ready to go!")
+            mapView.showsUserLocation = true
+            manager.startUpdatingLocation()
+        } else {
+            manager.requestWhenInUseAuthorization()
+        }
         
     }
-
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        //print("We made it")
+        if updateCount < 3 {
+            let region = MKCoordinateRegionMakeWithDistance(manager.location!.coordinate, 1000, 1000)
+            mapView.setRegion(region, animated: false)
+            updateCount += 1
+        }
+    }
 }
 
